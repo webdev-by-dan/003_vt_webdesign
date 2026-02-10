6915c8e98aefb9f6d411711183038319e3d29ae4
Daniel Scheuermann <webdev.by.dan@gmail.com>
2026-02-10 14:52:02 -0500

    x

--- src/js/main.js
+++ src/js/main.js
@@ -34,3 +34,10 @@
     })
   }
 }())
+
+const toggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.site-nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('is-open');
+});
