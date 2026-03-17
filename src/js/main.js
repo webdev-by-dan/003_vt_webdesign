cc4f6f77420976ba2629d0c82881325b7998330a
Daniel Scheuermann <webdev.by.dan@gmail.com>
2026-03-17 16:21:37 -0400

    Nav-Bar Redesign

--- src/js/main.js
+++ src/js/main.js
@@ -34,10 +34,3 @@
     })
   }
 }())
-
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.site-nav');
-
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('is-open');
-});
